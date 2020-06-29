3b0f672dc62535120d54c3daf67ea72f0e89a727
Michael Owolabi <michael.i.owolabi@gmail.com>
2020-06-29 08:29:20 +0100

    implement get user locale

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const unirest = require('unirest');
+require('dotenv').config();
+
+const app = express();
+const ipWhoisKey = process.env.IP_WHOIS_KEY;
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  
+  const apiCall = unirest(
+
+    "GET",
+ 
+    "https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/"
+ 
+  );
+ 
+  apiCall.headers({
+ 
+    "x-rapidapi-host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+ 
+    "x-rapidapi-key": ipWhoisKey
+ 
+  });
+ 
+  apiCall.end(function(result) {
+ 
+    if (res.error) throw new Error(result.error);
+ 
+    console.log(result.body);
+ 
+    res.send(result.body);
+ 
+  });
+
+})
+
+
+app.listen(port, () => {
+  console.log('server running')
+})
